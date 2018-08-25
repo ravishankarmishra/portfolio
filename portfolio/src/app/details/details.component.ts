e21ca6095085408d771dc4423b7aeb8c504021a4
Ravi <ravi.rsm77@gmail.com>
2018-08-26 00:36:32 +0530

    updated code to use scss

--- portfolio/src/app/details/details.component.ts
+++ portfolio/src/app/details/details.component.ts
@@ -8,7 +8,7 @@ export interface Skills {
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+  styleUrls: ['./details.component.scss']
 })
 
 export class DetailsComponent implements OnInit {
